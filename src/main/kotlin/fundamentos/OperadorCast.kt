755bb136999dcb35e9d66300d648f7d0ea53d8e3
yuri <ymrampazo@gmail.com>
2023-05-15 21:55:31 -0300

    Finalizando estudo de smartcast e operador cast

--- src/main/kotlin/fundamentos/OperadorCast.kt
+++ src/main/kotlin/fundamentos/OperadorCast.kt
@@ -2,8 +2,8 @@ package fundamentos
 
 fun imprimirConceito(nota: Any) {
 //  when (nota as Int) {
-//  when (nota as? Int ?: 5) {
-  when (nota as? Int) {
+  when (nota as? Int) {  //retorna null pra todas e cai no else
+//  when (nota as? Int ?: 5) {  //podemos usar elvis operator para caso de retornar null
     10, 9 -> println("A")
     8, 7 -> println("B")
     6, 5 -> println("C")
@@ -17,6 +17,7 @@ fun main(args: Array<String>) {
   val notas = arrayOf(9.6, 3.8, 7.2, 5.5, 4.1)
   for (nota in notas) {
 //imprimirConceito(nota) Gera erro de runtime classcastexception se não fizer cast seguro com safecall operator
+    imprimirConceito(nota)
     imprimirConceito(nota.toInt())
   }
 }
