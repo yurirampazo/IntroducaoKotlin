73557b7467608607cee5233fe8851d83152985ef
yuri <ymrampazo@gmail.com>
2023-06-07 00:04:01 -0300

    Collections: Equals e HashCode

---
+++ src/main/kotlin/collections/HashCodeEquals.kt
@@ -0,0 +1,34 @@
+package collections
+
+class Objeto(val nome: String, val descricao: String) {
+  override fun hashCode(): Int {
+    return nome.length
+  }
+
+  override fun equals(other: Any?) = if (other is Objeto) {
+    nome.equals(other.nome, ignoreCase = true)
+  } else {
+    false
+  }
+}
+
+fun main() {
+//  val list = arrayListOf("yue", 1, 'a', 3.14, true)
+//
+//  for (i in list) {
+//    if (i !is String) {
+//      println("$i is not a string")
+//    } else {
+//      println("$i is a string")
+//    }
+//  }
+  val conjunto = hashSetOf(
+    Objeto("Cadeira", "..."), //HashCode = 7
+    Objeto("Mesa", "..."), //HashCode = 4
+    Objeto("Faca", "..."), //HashCode = 4
+    Objeto("Copo", "...")
+  ) //HashCode = 4
+
+
+  conjunto.contains(Objeto("faca", "???")).print()
+}
